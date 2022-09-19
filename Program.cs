1366d8320b7cc476a74a7b5e126872f67e775b17
muccidavide <davidemucci0804@gmail.com>
2022-09-19 14:57:39 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Creare una classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+codice(numero intero)
+nome
+descrizione
+prezzo
+iva
+Usate opportunamente i livelli di accesso (public, private):
+i costruttori,
+i metodi getter e setter
+eventuali altri metodi di “utilità” per fare in modo che:
+alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+Il codice prodotto sia accessibile solo in lettura
+Gli altri attributi siano accessibili sia in lettura che in scrittura
+Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+Nella vostro programma principale, testate tutte le funzionalità della classe Prodotto.
+BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+Buon lavoro!*/
+
+Product[] allProducts = new Product[10];
+Console.WriteLine("Vuoi inserire o eliminare un prodotto?");
+string answer = Console.ReadLine();
+switch (answer)
+{
+    case "inserire":
+        Product newProduct = new Product();
+        newProduct.getAllInfo();
+        break;
+
+    case "eliminare":
+
+
+        break;
+
+}
+
+class Product
+{
+    int code;
+    string name;
+    string description; 
+    decimal price;
+    decimal iva = 20;
+
+    public Product()
+    {
+        var random = new Random();
+        code = random.Next(0, 100);
+        name = setName();
+        description = setDescription();
+        price = setPrice();
+
+
+    }
+
+    public string setName()
+    {
+        Console.WriteLine("Inserisci nome prodotto");
+        return name = Console.ReadLine();
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public string getFullName()
+    {
+        string fullName = code + name;
+        return fullName;
+    }
+
+    public string setDescription()
+    {
+        Console.WriteLine("Inserisci descrizione prodotto");
+        description = Console.ReadLine();
+        return description;
+    }
+
+    public string getDescription()
+    {
+        return description;
+    }
+
+    public decimal setPrice()
+    {
+        Console.WriteLine("Inserisci prezzo prodotto");
+        price = Convert.ToDecimal(Console.ReadLine());
+        return price;
+    }
+
+    public decimal getPrice()
+    {
+        Console.WriteLine($"Vuoi vedere il risultato con o senza IVA({iva}%)?");
+         string answerUser= Console.ReadLine();
+        switch (answerUser)
+        {
+            case "con":
+                decimal totalPrice = price + (price * iva / 100);
+                return totalPrice;
+
+            case "senza":
+                return price;
+                break ;
+        };
+
+        return price;
+    }
+
+    public void getAllInfo()
+    {
+
+        Console.WriteLine($"Nome: {getName()}");
+        Console.WriteLine($"Full name: {getFullName()}");
+        Console.WriteLine($"Descrizione: {getDescription()}");
+        Console.WriteLine($"Prezzo: {getPrice()}");
+
+    }
+
+}
